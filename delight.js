e07fb476ddbc4ce995585130f22aac221b1a204d
Axel Vermeil <axel.vermeil@gmail.com>
2017-03-17 15:03:17 +0000

    PLANS

--- delight.js
+++ delight.js
@@ -1,11 +1,46 @@
 ;(function(){
+	window.delightful_vars = {}
 	window.delight = {
-		generate : function (args, cb) {
-			cb('hello '+args.email)
+		variables : {},
+		generate : function (args) {
+			window.delightful_vars = args
 
-			
+			return this.getAllData().then(function (available_data) {
+				var city = available_data[0]
 
-			
+
+
+				var valid_messages = this.messages.filter(function (message) {
+					return //return if we have a all of sentence.needs in our api results
+				})
+
+
+
+				// pick a random message
+
+				// fill in the data using template thingy
+
+				// return final message
+
+				
+
+				return 'Loic is a very silly man'
+			})
+		},
+		messages : [{
+			text : 'Stay classy, $city',
+			needs : ['city']
+		},
+		{
+			text : 'Welcome back, $name',
+			needs : ['name']
+		}],
+
+		getAllData : function () {
+			return Promise.all([
+				getCity(),
+				getName()
+			])
 		}
 	}
 
